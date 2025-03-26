6ee69b2cbca5ee488a6610954f7220c7a61be474
kobikuz <kuzminsky.kobi@gmail.com>
2025-03-26 15:59:47 +0200

    Lagrange_new upload
    updated the lagrange file - the current and better version is lagrange_new!

--- Reed Solomon Codes/Lagrange_new.py
+++ Reed Solomon Codes/Lagrange_new.py
@@ -1,9 +1,6 @@
 from manim import *
 from copy import deepcopy 
-
-#### THATS THE OLD VERSION
-## THE ACTUAL VERSION IS THE LAGRANGE NEW!!
-class Lagrange_Interpulation(Scene):
+class Lagrange2(Scene):
     def func(self, x):
         return -x**2 + 4*x - 1
 
@@ -29,29 +26,32 @@ class Lagrange_Interpulation(Scene):
         points = VGroup()
         labels = VGroup()
         offsets = [0.75 * RIGHT, 0.75 * LEFT, 0.75 * LEFT]
+        def assign_dots(arr):
+            point_arr=VGroup()
+            label_arr = VGroup()
+            for i, (x, y) in enumerate(arr):
+                d = Dot(
+                    axes.c2p(x, y),
+                    radius=1 * DEFAULT_DOT_RADIUS, color=GREEN
+                )
 
-        for i, (x, y) in enumerate(coors):
-            d = Dot(
-                axes.c2p(x, y),
-                radius=1 * DEFAULT_DOT_RADIUS, color=GREEN
-            )
+                t = Tex("({%s}, {%s})" % (x, y))
+                t.next_to(d, DOWN)
+                t.shift(offsets[i])
 
-            t = Tex(f"({x}, {y})")
-            t.next_to(d, DOWN)
-            t.shift(offsets[i])
-
-            points.add(d)
-            labels.add(t)
-
+                point_arr.add(d)
+                label_arr.add(t)
+            return point_arr, label_arr
+        points, labels = assign_dots(coors)
+        #give copies because of the transformations
         labels_cp = labels.copy()
-
-
+        points_cp = points.copy()
+        ##
         c = ParametricFunction(
             lambda t: axes.c2p(t, self.func(t)),
             t_range=(0, 4.25), stroke_width=6, color=GREEN_A
         )
-        x_points = [(1,0), (3,0), (4,0)]
-        y_points = [(1,1), (3,1), (4,1)]
+
         eq = MathTex("p(x) = -x^2 + 4x - 1").scale(1.5).shift(3.25 * UP)
         eq_f = eq.copy()
         grp = VGroup(axes, c, points, labels)
@@ -65,7 +65,8 @@ class Lagrange_Interpulation(Scene):
         self.play(ApplyMethod(grp.shift, 0.5 * DOWN))
         self.play(Write(eq))
         self.wait()
-
+        self.play(ApplyWave(eq))
+        self.wait()
         self.play(FadeOut(VGroup(eq, c)))
 
         def l1(x): return (x-3)*(x-4)/6
@@ -73,80 +74,76 @@ class Lagrange_Interpulation(Scene):
             lambda t: axes.c2p(t, l1(t)),
             t_range=(0, 5), color=colors[0], stroke_width=6
         )
-        l1_cp = l1_c.copy()
-        p2l1_cp = l1_c.copy()
+
         def l2(x): return (x-1)*(x-4)/-2
         l2_c = ParametricFunction(
             lambda t: axes.c2p(t, l2(t)),
             t_range=(0, 5), color=colors[1], stroke_width=6
         )
-        l2_cp = l2_c.copy()
-        p2l2_cp = l2_c.copy()
+
         def l3(x): return (x-1)*(x-3)/3
         l3_c = ParametricFunction(
             lambda t: axes.c2p(t, l3(t)),
             t_range=(0, 5), color=colors[2], stroke_width=6
         )
-        l3_cp = l3_c.copy()
-        p2l3_cp = l2_c.copy()
+
         l_c = VGroup(l1_c, l2_c, l3_c)
-        l_cp = l_c.copy()
+        l_c_cp = l_c.copy()
 
         self.play(
             Write(l_c),
         )
+        self.wait(3) #i showed the 3 curves
+        self.play(FadeOut(l_c))
         self.wait()
+        coor1 = [(1,0), (3,0), (4,0)]
+        coor2 = [(1,1), (3,0), (4,0)]
+        coor3 = [(1,0), (3,1), (4,0)]
+        coor4 = [(1,0), (3,0), (4,1)]
+        coors_all = [coor1, coor2, coor3, coor4]
+        labels_all =[]
+        dots_all = []
+        for i in range(4):
+            labels_in = VGroup()
+            dots = VGroup()
+            dots, labels_in = assign_dots(coors_all[i])
+            labels_all.append(labels_in)
+            dots_all.append(dots)
+        original_dots = dots_all[0].copy()
+        original_labels = labels_all[0].copy()
+        self.play(Transform(labels,labels_all[0]),Transform(points,dots_all[0]))
+        self.wait()
+        self.play(
+            Write(l_c[0]),
+            Transform(labels, labels_all[1]),
+            Transform(points, dots_all[1])
+        )
 
-    ## DEFINING FUNCTIONS
-        def hide_all_but(curr_idx):
-            anims = []
-            for i in range(3):
-                if i != curr_idx:
-                    anims.append(FadeOut(l_c[i]))  # Completely fade out other graphs
-            self.play(*anims)
-
-        def show_all():
-            anims = [FadeIn(l_c[i]) for i in range(3)]  # Restore graphs
-            self.play(*anims)
-
-        def one_hot(x_val, curve_index):
-            return 1 if x_val in x_vals and x_val == x_vals[curve_index] else 0
-
-        def show_curve(curve_index):
-            hide_all_but(curve_index)  # Hide other graphs
-
-            points_0 = VGroup()
-            for x in x_vals:
-                points_0.add(
-                    Dot(axes.c2p(x, one_hot(x, curve_index)), radius=0.1, color=colors[curve_index])
-                )
-
-            labels_0 = VGroup()
-            for i in range(3):
-                labels_0.add(
-                    Tex(f"({x_vals[i]}, {one_hot(x_vals[i], curve_index)})").next_to(points_0[i], UP)
-                )
-
-            self.play(Write(points_0), Transform(labels, labels_0))
+        def indicate_y(label,one_hot_idx):
+            rest_anims=[]
+            for j in range(3):
+                k=0
+                if j==2:
+                    k=1
+                if(j!=one_hot_idx):
+                    rest_anims.append(Indicate(label[j][0][3+k]))
+                elif(j==one_hot_idx):
+                    self.play(Indicate(label[one_hot_idx][0][3+k]))                
+            self.wait(1)
+            self.play(*rest_anims)
+        self.wait()    
+        indicate_y(labels,0)
+        self.wait()
+        for i in range(1,3):
+            self.play(Transform(l_c[0],l_c[i]),Transform(labels,labels_all[i+1]),Transform(points,dots_all[i+1]), run_time= 1.5)
+            indicate_y(labels,i)
             self.wait()
+        self.play(Transform(labels,original_labels),Transform(points,original_dots),FadeOut(l_c[0]))
 
-            self.play(Indicate(labels[curve_index]))
-            self.wait()
 
-            self.play(
-                Transform(labels, labels_cp),
-                Uncreate(points_0)  # Remove points smoothly
-            )
-            #show_all()  # Restore all graphs
-
-            self.wait()
-
-    ## RUNNING THE FUNCTIONS
-        show_curve(0)
-        show_curve(1)
-        show_curve(2)
-
-##new part
+        ##new part
+        final_point_coor = [(1,2), (3,2), (4,-1)]
+        final_point_dots, final_point_labels = assign_dots(final_point_coor)
         def p2_l1(x): return (x-3)*(x-4)
         p2_l1_c = ParametricFunction(
             lambda t: axes.c2p(t, p2_l1(t)),
@@ -163,7 +160,6 @@ class Lagrange_Interpulation(Scene):
             t_range=(0.2, 4.75), color=colors[1], stroke_width=6
         )
         
-        self.play(Uncreate(l_c))
         self.wait()
         eq10 = MathTex(
             r"l_1(x) = (x-3) (x-4)"
@@ -260,17 +256,21 @@ class Lagrange_Interpulation(Scene):
         eq_arr_result = [eq_arr_result_1,eq_arr_result_2,eq_arr_result_3]
         p2_funcs1 = [p2_l1_1_c,p2_l2_1_c,p2_l3_1_c]
         eq_after_x_tex = [eq20,eq21,eq22]
-        eq_mult_y = [p2_l1_1f_c,p2_l2_1f_c,p2_l3_1f_c]
-        eq_mult_y_tex = [eq20f,eq21f,eq22f]
+
         
 
         for i in range(3):
-            self.play(Write(eq_arr_first[i]))
+            k=0
+            self.play(Write(eq_arr_first[i]),run_time = 1.5)
             self.play(Write(p2_funcs0[i]))## basic func
             self.wait(0.5)
-            self.play(Indicate(labels[i][0][1]))
-            self.wait(0.5)
-            self.play(Transform(eq_arr_first[i],eq_arr_second[i]),run_time = 1)## calculating
+            if i==2:
+                k=1
+            self.play(Indicate(labels[i][0][1+k]))
+            moving_number = labels[i][0][1+k].copy()
+            self.add(moving_number)
+            self.play(moving_number.animate.move_to(eq_arr_first[i].get_center()).set_opacity(0),
+            Transform(eq_arr_first[i],eq_arr_second[i]),run_time = 1.5)
             self.wait(0.5)
             self.play(Write(eq_arr_result[i].next_to(eq_arr_first[i],RIGHT).set_color(colors[i])))## result
             self.wait(0.5)
@@ -283,22 +283,32 @@ class Lagrange_Interpulation(Scene):
             self.play(
                 Transform(p2_funcs0[i], p2_funcs1[i])  # Transform the function curve
                 )
-            self.wait(1)
+            self.wait(3)
+
+        #now lets return the original points and make the polinomials go through them
+        self.play(Transform(points,final_point_dots),Transform(labels,final_point_labels))
+        self.wait(2)
+        eq_mult_y = [p2_l1_1f_c,p2_l2_1f_c,p2_l3_1f_c]
+        eq_mult_y_tex = [eq20f,eq21f,eq22f]
+        for i in range(3):
             if i==2:
-                self.play(Indicate(labels[i][0][3:5]))
-                self.play(TransformMatchingTex(eq_after_x_tex[i],eq_mult_y_tex[i]))
+                self.play(Indicate(labels[i][0][4]))
+                moving_num = labels[i][0][4].copy() 
+                self.play(moving_num.animate.move_to(eq_arr_first[i].get_center()).set_opacity(0),
+                Transform(eq_after_x_tex[i],eq_mult_y_tex[i]),run_time= 1.5)
                 self.wait(0.5)
                 self.play(Transform(p2_funcs0[i],eq_mult_y[i]))
             else: 
                 self.play(Indicate(labels[i][0][3]))
-                self.play(TransformMatchingTex(eq_after_x_tex[i],eq_mult_y_tex[i]))
+                moving_num = labels[i][0][3].copy()
+                self.play(moving_num.animate.move_to(eq_arr_first[i].get_center()).set_opacity(0),
+                          Transform(eq_after_x_tex[i],eq_mult_y_tex[i]),run_time= 1.5)
                 self.wait(0.5)
                 self.play(Transform(p2_funcs0[i],eq_mult_y[i]))
-                
-            self.wait(1)
-
         self.wait(1.5)
         all_curves = VGroup(p2_funcs0[0],p2_funcs0[1],p2_funcs0[2])
-        all_eq = VGroup(eq_mult_y_tex[0],eq_mult_y_tex[1],eq_mult_y_tex[2])
-        self.play(Transform(all_curves,c),TransformMatchingTex(all_eq,eq_f),run_time = 2)
-        self.wait(1.5)
+        all_eq = VGroup(eq_after_x_tex[0],eq_after_x_tex[1],eq_after_x_tex[2])
+        self.play(Transform(all_curves,c),Transform(all_eq,eq_f),run_time = 2)
+        self.wait(1)
+        self.play(ApplyWave(eq_f),run_time= 2)
+        self.wait(2)
